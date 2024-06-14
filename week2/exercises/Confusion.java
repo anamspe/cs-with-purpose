8aacc818505a32fa644bed67eaf093ab2fbbd74b
Ana Franco <anamspe.fr@gmail.com>
2024-06-13 21:02:34 -0700

    Loops and conditionals exercises

---
+++ week2/exercises/Confusion.java
@@ -0,0 +1,16 @@
+public class Confusion {
+
+    public static void main(String[] args) {
+        int i = 10;
+        System.out.println("i = " + i);
+
+        i = i++;
+        System.out.println("(i = i++) = " + i);
+
+        i = ++i;
+        System.out.println("(i = ++i) = " + i);
+
+        i = i++ + ++i;
+        System.out.println("(i = i++ + ++i) = " + i);
+    }
+}
